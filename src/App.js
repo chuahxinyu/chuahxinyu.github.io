32473ed777392f737789fc42bef7919a1420387a
Xin Yu <66938425+chuahxinyu@users.noreply.github.com>
2022-02-05 11:12:26 +0800

    V1 - Plain Projects

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.scss';
+import Projects from './components/Projects';
+
+function App() {
+  return (
+    <div className="App">
+      <Projects />
+    </div>
+  );
+}
+
+export default App;
