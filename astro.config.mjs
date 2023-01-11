bc95db266c50591f67393ad7a6d7e0ee7b9fe6b0
Xin Yu <chuaxc@unimelb.edu.au>
2023-01-11 16:28:27 +0800

    Added MDX and Tailwind integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,12 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 
 // https://astro.build/config
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react(), mdx(), tailwind()]
 });
