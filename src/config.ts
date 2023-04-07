43e49ea07b3c43d44baa8edc94e946b50ede47e6
Xin Yu <chuaxc@unimelb.edu.au>
2023-04-07 13:37:46 +1000

    Added Hover Tooltips and Fixed Hero Responsiveness

--- src/config.ts
+++ src/config.ts
@@ -2,11 +2,11 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Xin Yu";
-export const SITE_DESCRIPTION =
-  "Xin Yu's little corner of the internet.";
+export const SITE_DESCRIPTION = "Xin Yu's little corner of the internet.";
 // export const TWITTER_HANDLE = "@yourtwitterhandle";
 export const MY_NAME = "Xin Yu";
 export const GITHUB_USERNAME = "chuahxinyu";
+export const LINKEDIN_USERNAME = "chuahxinyu";
 
 // setup in astro.config.mjs
 const BASE_URL = new URL(import.meta.env.SITE);
