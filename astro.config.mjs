69f113dadd73ab28681cf52b55c2b17ac7972741
Xin Yu <chuaxc@unimelb.edu.au>
2023-11-05 17:01:36 +1100

    Added MDX, Tailwind, react, Katex, DaisyUI

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,18 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://chuahxinyu.github.io",
+  integrations: [react(), tailwind(), mdx(), sitemap()],
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
+});
---
+++ src/layouts/Layout.tsx
@@ -0,0 +1,5 @@
+function Layout({ children }: { children: React.ReactNode }) {
+  return <div className="flex min-h-screen flex-col">{children}</div>;
+}
+
+export default Layout;
