5bda8d2c32284f239afb9b95189b70262a9941f4
Xin Yu <chuaxc@unimelb.edu.au>
2023-11-06 22:02:47 +1100

    Reverted Next Version to 13.5.1 and Added next-contentlayer

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
+const withContentlayer = require("next-contentlayer").withContentlayer;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ["js", "jsx", "ts", "tsx", "mdx"],
-  experimental: {
-    mdxRs: true,
-  },
+  reactStrictMode: true,
+  swcMinify: true,
+  // Optionally, add any other Next.js config below
 };
 
-const withMDX = require("@next/mdx")();
-module.exports = withMDX(nextConfig);
+module.exports = withContentlayer(nextConfig);
